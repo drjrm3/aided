69197f308c0411a6115fc443796b7290fcb6db4d
J. Robert Michael, PhD <jrobert.michael@gmail.com>
2024-11-25 14:58:13 -0600

    MAINT: Initial readme.

---
+++ src/aided/cli.py
@@ -0,0 +1,24 @@
+"""
+"""
+
+import argparse
+import sys
+from typing import List
+
+def get_args(argv: List[str]):
+    """
+    TODO:
+    """
+
+    parser = argparse.ArgumentParser("aided")
+
+    parser.add_argument("-w", "--wfn-files", nargs="+", type=str, help="WFN(s)")
+    parser.add_argument("-c", "--config-file", type=str, help="JSON config file.")
+
+def main():
+    args = get_args(sys.argv[1:])
+
+
+
+if __name__ == "__main__":
+    main()
